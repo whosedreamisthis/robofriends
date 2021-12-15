3a11a7740eb08dc17f255165983a83543f634512
Whose dream is this? <whosedreamisthis@gmail.com>
2021-12-14 17:44:03 -0800

    robofriends Zero to Mastery course project

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css'
+import Scroll from '../components/Scroll.js'
+   // <SearchBox />
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            return response.json();
+        })
+        .then(users => {
+                this.setState({robots:users})
+        });
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value.toLowerCase()})
+
+
+        //console.log(filteredRobots);
+
+    }
+    render () {
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(searchfield)
+        })
+        return !robots.length ?
+            <h1>Loading...</h1> :
+        
+        (<div className ='tc'>
+                    <h1 className='f1' >RoboFriends</h1>
+                    <SearchBox searchfield={searchfield} searchchange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>);
+        
+    }
+}
+
+export default App;
